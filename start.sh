b6fde8590268d3dcfb97c73f04957ba4e2c3e9a3
Moeto88 <sonodam@tcd.ie>
2023-04-10 22:14:20 +0100

    test

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+until ncat -z -v -w30 db 5432
+do
+echo "Waiting for database connection for 5 seconds..."
+
+## Wait for 5 seconds before check again. test
+sleep 5
+done
+echo "Database server ready..."
 gunicorn app:app -b 0.0.0.0:5297 -w 8 -k uvicorn.workers.UvicornWorker & yarn dev
